478133645fdeb2b59d63b7ce4f90b5be05a2ba99
Almir Santos <almirpask@hotmail.com>
2017-07-23 00:11:55 -0300

    form e table

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';    
 
+let data = [
+    {idNumber: "1", name:"Maria",email:"maria@mail.com",subject:"R", messenger:"mensagem teste" },
+    {idNumber: "2", name:"Perdro",email:"perdro@mail.com",subject:"A", messenger:"mensagem teste" },
+    {idNumber: "3", name:"Gustavo",email:"gustavo@mail.com",subject:"E", messenger:"mensagem teste" }
+]
+
 let Nav = React.createClass({
     render:function(){
         return (
@@ -21,10 +27,16 @@ let Nav = React.createClass({
 });
 
 let Title = React.createClass({
+    
     render:function(){
+        let style = {
+            color: "#777676",
+            fontSize: "50px"
+
+        }
         return (
                 <div className="row">
-                    <h1>{this.props.title}</h1>
+                    <h1 style={style}>{this.props.children}</h1>
                 </div>
         )
     }
@@ -47,7 +59,7 @@ let Button = React.createClass({
         let  btnClass = this.state.click? 'btn btn-success' : 'btn btn-default';
         let  textActive = this.state.click? 'Enviado!' : this.props.textActive;
         return(
-            <button onClick={ this.toggleClick } className={btnClass}> {textActive}</button>
+            <button onClick={ this.toggleClick } className={btnClass}> {this.props.children}</button>
         )
     }
 })
@@ -63,6 +75,15 @@ let Form = React.createClass({
                  <div className="form-group">
                     <label htmlFor="email">Email</label>
                     <input type="email" id="email" className="form-control"/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="subject">Assunto</label>
+                    <select defaultValue="R" name="subject" id="subject" className="form-control">
+                        <option value="A">Angular</option>
+                        <option value="J">Jquery</option>
+                        <option value="R">React</option>
+                        <option value="E">Ember</option>
+                    </select>
                 </div>
                  <div className="form-group">
                     <label htmlFor="messenger">Messenger</label>
@@ -80,14 +101,62 @@ let Page = React.createClass({
             <myElement>
                 <Nav title="Teste Props"/>
                 <div className="container">
-                    <Title title="Teste Props"/>
+                    <Title>
+                        Teste
+                    </Title>
                     <div className="row">
                         <Form/>
-                        <Button textActive="Send"/>
+                        <Button>Send</Button>  
                     </div>  
+                    <div className="row">
+                        <List data={data}/>
+                    </div>
                 </div>
             </myElement>
         );
+    }
+});
+
+let Contact = React.createClass({
+    render: function(){
+        return(
+            <tr>
+                <td scope="row">{this.props.idNumber}</td>
+                <td>{this.props.name}</td>
+                <td>{this.props.email}</td>
+                <td>{this.props.subject}</td>
+                <td>{this.props.children}</td>
+            </tr>
+        )
+    }
+})
+
+let List = React.createClass({
+
+    render: function(){
+        let contactNodes = this.props.data.map(function(contact){
+            return(
+                <Contact idNumber={contact.idNumber} name={contact.name} email={contact.email} subject={contact.subject}>
+                    {contact.messenger}
+                </Contact>
+            );
+        })
+        return(
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Email</th>
+                        <th>Assunto</th>
+                        <th>Mensagem</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {contactNodes}
+                </tbody>
+            </table>
+        )
     }
 });
 //ReactDOM.render(<App />, document.getElementById('root'));
